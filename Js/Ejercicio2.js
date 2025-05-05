fda76b9fe1806b07562fc9388298fdc050c8926b
Santiago Lacki <santiagolacki@gmail.com>
2025-05-05 16:55:36 -0300

    Se agrego el enunciado y una resolucion parcial del ejercicio2 en su respectivo archivo

---
+++ Js/Ejercicio2.js
@@ -0,0 +1,42 @@
+
+// 2-Escribe un programa que cree un objeto 
+// "cuenta" con las siguientes propiedades:
+// Una propiedad titular con el valor "Alex".
+// Una propiedad saldo, teniendo como valor 
+// inicial 0.
+// Un método ingresar() que permita añadir 
+// dinero a la cuenta, pasando la cantidad 
+// como parámetro
+// Un método extraer() que permita retirar la 
+// cantidad pasada como parámetro.
+// Un método informar() que retorne la 
+// información del estado de la cuenta. 
+// Utiliza este objeto para mostrar la 
+// descripción, ingresar y extraer dinero y 
+// volver a mostrar la descripción del estado 
+// de la cuenta.
+
+const cuenta = {
+  titular: "Alex",
+  saldo: 0,
+  
+  ingresar: (cantidad) => {
+    this.saldo = this.saldo + cantidad
+    console.log(this.saldo)
+  },
+
+  extraer: (cantidad) => {
+    this.saldo = this.saldo - cantidad
+    console.log(this.saldo)
+  },
+
+  informar: () => {
+    document.writeln("Estado de la cuenta: <br>");
+    document.writeln(`Titular: ${this.titular} <br>`);
+    document.writeln("Saldo actual: ", this.saldo, "<br>");
+  },
+}
+
+cuenta.ingresar(1000);
+cuenta.extraer(500);
+cuenta.informar();
